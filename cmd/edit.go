f5756ec1d8522d631b2586a87bf3c607d668ce82
b4b4r07 <b4b4r07@gmail.com>
2017-06-01 20:27:39 +0900

    Do not sync if you do not edit

--- cmd/edit.go
+++ cmd/edit.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"io/ioutil"
+
 	"github.com/knqyf263/pet/config"
 	"github.com/spf13/cobra"
 )
@@ -17,11 +19,32 @@ func edit(cmd *cobra.Command, args []string) (err error) {
 	editor := config.Conf.General.Editor
 	snippetFile := config.Conf.General.SnippetFile
 
+	// file content before editing
+	before := fileContent(snippetFile)
+
 	err = editFile(editor, snippetFile)
-	if err == nil && config.Conf.Gist.AutoSync {
+	if err != nil {
+		return
+	}
+
+	// file content after editing
+	after := fileContent(snippetFile)
+
+	// return if same file content
+	if before == after {
+		return nil
+	}
+
+	if config.Conf.Gist.AutoSync {
 		return autoSync(snippetFile)
 	}
-	return err
+
+	return nil
+}
+
+func fileContent(fname string) string {
+	data, _ := ioutil.ReadFile(fname)
+	return string(data)
 }
 
 func init() {
