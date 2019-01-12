31df594a51f19868d3924f548716c6337af04eb4
Matthias Langhard <matthias@langhard.com>
2019-01-12 08:56:41 +0100

    Add config option to sort snippets (#88)

--- snippet/snippet.go
+++ snippet/snippet.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"sort"
 
 	"github.com/BurntSushi/toml"
 	"github.com/knqyf263/pet/config"
@@ -29,6 +30,7 @@ func (snippets *Snippets) Load() error {
 	if _, err := toml.DecodeFile(snippetFile, snippets); err != nil {
 		return fmt.Errorf("Failed to load snippet file. %v", err)
 	}
+	snippets.Order()
 	return nil
 }
 
@@ -52,3 +54,52 @@ func (snippets *Snippets) ToString() (string, error) {
 	}
 	return buffer.String(), nil
 }
+
+// Order snippets regarding SortBy option defined in config toml
+// Prefix "-" reverses the order, default is "recency", "+<expressions>" is the same as "<expression>"
+func (snippets *Snippets) Order() {
+	sortBy := config.Conf.General.SortBy
+	switch {
+	case sortBy == "command" || sortBy == "+command":
+		sort.Sort(ByCommand(snippets.Snippets))
+	case sortBy == "-command":
+		sort.Sort(sort.Reverse(ByCommand(snippets.Snippets)))
+
+	case sortBy == "description" || sortBy == "+description":
+		sort.Sort(ByDescription(snippets.Snippets))
+	case sortBy == "-description":
+		sort.Sort(sort.Reverse(ByDescription(snippets.Snippets)))
+
+	case sortBy == "output" || sortBy == "+output":
+		sort.Sort(ByOutput(snippets.Snippets))
+	case sortBy == "-output":
+		sort.Sort(sort.Reverse(ByOutput(snippets.Snippets)))
+
+	case sortBy == "-recency":
+		snippets.reverse()
+	}
+}
+
+func (snippets *Snippets) reverse() {
+	for i, j := 0, len(snippets.Snippets)-1; i < j; i, j = i+1, j-1 {
+		snippets.Snippets[i], snippets.Snippets[j] = snippets.Snippets[j], snippets.Snippets[i]
+	}
+}
+
+type ByCommand []SnippetInfo
+
+func (a ByCommand) Len() int           { return len(a) }
+func (a ByCommand) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByCommand) Less(i, j int) bool { return a[i].Command > a[j].Command }
+
+type ByDescription []SnippetInfo
+
+func (a ByDescription) Len() int           { return len(a) }
+func (a ByDescription) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByDescription) Less(i, j int) bool { return a[i].Description > a[j].Description }
+
+type ByOutput []SnippetInfo
+
+func (a ByOutput) Len() int           { return len(a) }
+func (a ByOutput) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByOutput) Less(i, j int) bool { return a[i].Output > a[j].Output }
