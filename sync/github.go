17a91f5317c73f84db0f54b5f4a18fafc9607bea
Leandro Deveikis <leandro.deveikis@gmail.com>
2023-12-06 08:41:16 -0300

    minor improvements

--- sync/github.go
+++ sync/github.go
@@ -1,7 +1,6 @@
 package sync
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"github.com/knqyf263/pet/config"
@@ -22,7 +21,7 @@ func NewGithubClient() (Client, error) {
 	accessToken, err := getGithubAccessToken()
 	if err != nil {
 		return nil, fmt.Errorf(`access_token is empty.
-Go https://github.com/settings/tokens/new and create access_token (only need "gist" scope).
+Go https://github.com/settings/tokens/new and create access_token (only need "repo" scope).
 Write access_token in config file (pet configure) or export $%v.
 		`, githubTokenEnvVariable)
 	}
@@ -41,77 +40,53 @@ func (g GithubClient) GetSnippet() (*Snippet, error) {
 	defer s.Stop()
 
 	ghConfig := config.Conf.GitHub
-	// todo add timeout?
-	content, err := g.Client.Repositories.DownloadContents(context.Background(), ghConfig.RepoOwner, ghConfig.RepoName, ghConfig.FileName, nil)
+	fileContent, _, resp, err := g.Client.Repositories.GetContents(context.Background(), ghConfig.RepoOwner, ghConfig.RepoName, ghConfig.FileName, nil)
 	if err != nil {
+		fmt.Printf("Error from Github: %s", resp.Status)
 		return nil, errors.Wrapf(err, "Failed to get repo")
 	}
 
-	buf := new(bytes.Buffer)
-	_, err = buf.ReadFrom(content)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed read github response")
-	}
-
 	return &Snippet{
-		Content: buf.String(),
+		Content: *(fileContent.Content),
 	}, nil
 }
 
 // UploadSnippet uploads local snippets to Github
 func (g GithubClient) UploadSnippet(content string) error {
-	ghConfig := config.Conf.GitHub
-	// get SHA to be able to update the file
-	sha := ""
-	opt := &github.CommitsListOptions{
-		Path: *github.String(ghConfig.FileName),
-	}
-	commits, _, _ := g.Client.Repositories.ListCommits(context.Background(), ghConfig.RepoOwner, ghConfig.RepoName, opt)
-	t, _, _ := g.Client.Git.GetTree(context.Background(), ghConfig.RepoOwner, ghConfig.RepoName, commits[0].GetSHA(), true)
-	for _, entry := range t.Entries {
-		if *entry.Path == *github.String(ghConfig.FileName) {
-			sha = *entry.SHA
-		}
-	}
+	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+	s.Start()
+	s.Suffix = " Uploading configuration to Github..."
+	defer s.Stop()
+
+	// input data
+	owner, repoName, fileName := config.Conf.GitHub.RepoOwner, config.Conf.GitHub.RepoName, config.Conf.GitHub.FileName
+	// we need the SHA to be able to update the file
+	sha := getShaForFile(fileName, owner, repoName, g)
 	// update content
-	fileContent := []byte(content)
 	opts := &github.RepositoryContentFileOptions{
 		Message: github.String("Updating snippet configuration"),
-		Content: fileContent,
+		Content: []byte(content),
 		Branch:  github.String("main"),
-		//Committer: &github.CommitAuthor{Name: github.String("FirstName LastName"), Email: github.String("user@example.com")},
-		SHA: &sha,
+		SHA:     &sha,
 	}
-	_, _, err := g.Client.Repositories.CreateFile(context.Background(), ghConfig.RepoOwner, ghConfig.RepoName, ghConfig.FileName, opts)
+	_, _, err := g.Client.Repositories.CreateFile(context.Background(), owner, repoName, fileName, opts)
 	if err != nil {
 		return errors.Wrap(err, "Failed to upload changes to github")
 	}
 	return nil
 }
 
-/**
-func (g GithubClient) createGist(ctx context.Context, gist *github.Gist) (gistID *string, err error) {
-	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
-	s.Start()
-	s.Suffix = " Creating Gist..."
-	defer s.Stop()
-
-	retGist, _, err := g.Client.Gists.Create(ctx, gist)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to create gist")
+func getShaForFile(fileName string, owner string, repoName string, g GithubClient) string {
+	sha := ""
+	opt := &github.CommitsListOptions{
+		Path: *github.String(fileName),
 	}
-	return retGist.ID, nil
-}
-
-func (g GithubClient) updateGist(ctx context.Context, gist *github.Gist) (err error) {
-	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
-	s.Start()
-	s.Suffix = " Updating Gist..."
-	defer s.Stop()
-
-	if _, _, err = g.Client.Gists.Edit(ctx, g.ID, gist); err != nil {
-		return errors.Wrap(err, "Failed to edit gist")
+	commits, _, _ := g.Client.Repositories.ListCommits(context.Background(), owner, repoName, opt)
+	t, _, _ := g.Client.Git.GetTree(context.Background(), owner, repoName, commits[0].GetSHA(), true)
+	for _, entry := range t.Entries {
+		if *entry.Path == *github.String(fileName) {
+			sha = *entry.SHA
+		}
 	}
-	return nil
+	return sha
 }
-*/
