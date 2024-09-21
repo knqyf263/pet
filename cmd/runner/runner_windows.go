9b250c7dadd5dcd1a265971736cee8b84e135233
Pedro Augusto Campos Dos Santos <38117637+ppcamp@users.noreply.github.com>
2024-09-21 15:40:13 -0300

    refactor: create a module and export Run function

---
+++ cmd/runner/runner_windows.go
@@ -0,0 +1,28 @@
+//go:build windows
+
+package runner
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"syscall"
+
+	"github.com/knqyf263/pet/config"
+)
+
+func Run(command string, r io.Reader, w io.Writer) error {
+	var cmd *exec.Cmd
+	if len(config.Conf.General.Cmd) > 0 {
+		line := append(config.Conf.General.Cmd, command)
+		cmd = exec.Command(line[0], line[1:]...)
+	} else {
+		cmd = exec.Command("cmd.exe")
+		cmd.SysProcAttr = &syscall.SysProcAttr{CmdLine: fmt.Sprintf("/c \"%s\"", command)}
+	}
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = w
+	cmd.Stdin = r
+	return cmd.Run()
+}
