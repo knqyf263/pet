80dca3b61b010871111bad9876f278573551a355
Taylan Kasap <taylankasap@gmail.com>
2024-11-03 20:06:15 +0300

    refactor: Move toml regexp to package scope to make it testable

---
+++ snippet/util_test.go
@@ -0,0 +1,37 @@
+package snippet
+
+import (
+	"testing"
+)
+
+func TestTomlRegEx(t *testing.T) {
+	tests := []struct {
+		name string
+		path string
+		want bool
+	}{
+		{name: "match - filename", path: "snippet.toml", want: true},
+		{name: "match - absolute path", path: "/home/username/.config/pet/config.toml", want: true},
+		{name: "match - absolute path with home alias", path: "~/.config/pet/snippet2.toml", want: true},
+		{name: "match - relative path", path: "../../some/directory/best.toml", want: true},
+		{name: "mismatch - filename", path: "file.yaml", want: false},
+		{name: "mismatch - absolute path", path: "/home/username/.config/pet/config.json", want: false},
+		{name: "mismatch - absolute path with home alias", path: "~/.config/pet/snippet2.xml", want: false},
+		{name: "mismatch - relative path", path: "../../some/directory/unrelated.html", want: false},
+		{name: "mismatch - extension with dot", path: ".toml", want: false},
+		{name: "mismatch - extension without dot", path: "toml", want: false},
+		{name: "mismatch - similar extension prefix", path: "file.tomlx", want: false},
+		{name: "mismatch - similar extension suffix", path: "file.xtoml", want: false},
+		{name: "mismatch - similar extension infix", path: "file.xtomlx", want: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tomlRegEx.MatchString(tt.path)
+
+			if got != tt.want {
+				t.Errorf("Expected result %v, but got %v", tt.want, got)
+			}
+		})
+	}
+}
